503f85c163c0befd969aff50b412093db03df2c1
Siavash <skhodaka@node2323.oscar.ccv.brown.edu>
2026-05-20 10:41:38 -0400

    Update jobs

--- Kdv/Pytorch/job_PIKAN.sh
+++ Kdv/Pytorch/job_PIKAN.sh
@@ -1,18 +1,18 @@
-#!/bin/bash
-#SBATCH -J KdvPikan
-#SBATCH --time=12:00:00
-#SBATCH -N 1
-#SBATCH -n 8
-#SBATCH --mem=32g
-#SBATCH -A gk-h100-gcondo
-#SBATCH -p h100-gcondo
-#SBATCH --gres=gpu:1
-#SBATCH -o PIKAN_%j.out
-#SBATCH -e PIKAN_%j.err
+## Slurm job example
 
-source /users/skhodaka/gpu_env.venv/bin/activate
+#!/bin/bash
+#SBATCH -J "Specify job name"
+#SBATCH --time=3:00:00 "Specify time"
+#SBATCH -N -N "Specify Node number"
+#SBATCH -n "Specify CPU number"
+#SBATCH --mem= "Specify Memory"
+#SBATCH "Specify GPU"
+#SBATCH --gres=gpu:1 "Specify GPU number"
+#SBATCH -o PIKAN_%j.out
+
+source "Specfiy Pytorch environment (if needed)"
 
 python -u PIKAN.py train.adam_epochs=100000 train.learning_rate=1e-3 train.checkpoint_name="PIKAN" train.checkpoint_interval=1000 train.epoch_resample=25000 \
         model.degree=7 model.num_layer=4 model.width_layer=64  model.skip_con=False weights.w1_org=1.0 weights.w2_org=25.0 train.double_precision=True  train.L1_epoch=200000 \
-        model.seed=1
+        model.seed=42
 
--- Kdv/Pytorch/job_PINN.sh
+++ Kdv/Pytorch/job_PINN.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-#SBATCH -J pikdv_soap_SIREN
+#SBATCH -J pikdv
 #SBATCH --time=2:00:00
-#SBATCH -N 1
-#SBATCH -n 4
-#SBATCH --mem=32g
-## SBATCH -A gk-h100-gcondo
-## SBATCH -p a6000-gcondo
-#SBATCH -A gk-l40s-gcondo
-#SBATCH -p l40s-gcondo
-#SBATCH --gres=gpu:1
-#SBATCH -o PINN_Soap_SIREN_%j.out
-#SBATCH -e PINN3_Soap_SIREN_%j.err
+#SBATCH -N "Specify Node number"
+#SBATCH -n "Specify CPU number"
+#SBATCH --mem= "Specify Memory"
+#SBATCH -"Specify GPU"
+#SBATCH --gres=gpu:1 "Specify GPU number"
+#SBATCH -o PINN_%j.out
 
-source /users/skhodaka/gpu_env.venv/bin/activate
+source "Specfiy Pytorch environment (if needed)"
 
 python -u PINN.py train.adam_epochs=100000 train.learning_rate=1e-3 train.checkpoint_name="PINN_soap_SIREN" train.checkpoint_interval=1000 train.epoch_resample=25000 model.features="[100,100,100,100,100,100]" \
         model.skip_con=False weights.w1_org=1.0 weights.w2_org=25.0 train.double_precision=True  train.L1_epoch=200000 model.use_siren=True model.siren_w0=30.0 model.learning_w0=False \
